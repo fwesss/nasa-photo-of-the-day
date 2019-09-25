048dd4836eb316c07a105dbd27a45f6b290d8dd3
Westley Feller <feller.westley@gmail.com>
2019-09-24 19:37:49 -0700

    Project files linted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun
+        {' '}
+        <span role="img" aria-label="rocket emoji">🚀</span>
+!
       </p>
     </div>
   );
